f2813bb26e8056d6d6ebc57e4dd75f2750e8e6ed
AslDev006 <asldev006@gmail.com>
2024-05-20 13:49:31 +0500

    DTBASE

--- models/models.py
+++ models/models.py
@@ -44,6 +44,6 @@ class MovieModels(models.Model):
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
     key = models.CharField(max_length=255, unique=True, null=True, blank=True)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     def __str__(self):
         return f"{self.key} {self.title}"
